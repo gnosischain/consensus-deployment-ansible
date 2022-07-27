cd414dcf7627deb18ad623b1ab623ced3ff29261
dapplion <35266934+dapplion@users.noreply.github.com>
2022-07-27 18:07:03 +0200

    Generate genesis

--- chiado/scripts/generate_consensus_state.sh
+++ chiado/scripts/generate_consensus_state.sh
@@ -7,6 +7,7 @@ docker run --entrypoint=eth2-testnet-genesis \
 -v $PWD/chiado/custom_config_data:/data \
 skylenet/ethereum-genesis-generator phase0 \
 --eth1-block 0x0000000000000000000000000000000000000000000000000000000000000000 \
+--timestamp 1660132800 \
 --config /data/config.yaml \
 --mnemonics /data/mnemonics.yaml \
 --tranches-dir /data/tranches \
